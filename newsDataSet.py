285e9da725bafd384a26931b4440d7f6551f94cc
de-wei ye <deweiye6626@gmail.com>
2019-05-26 15:00:54 +0800

    [fix] typo

--- newsDataSet.py
+++ newsDataSet.py
@@ -1,5 +1,13 @@
+import re
+
+
+import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from pytorch_pretrained_bert import BertTokenizer
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 
 
 class TrainingQuery(Dataset):
@@ -11,6 +19,7 @@ class TrainingQuery(Dataset):
         
         # proccess data
         merged_training = pd.merge(raw_training_data, news_urls, on=['News_Index'])
+        contents = contents.apply(lambda x: ' [CLS] ' + re.sub('[\n，。、]+', ' [SEP] ', x))
         
         # construct dataset
         self.queries = merged_training['Query']
@@ -20,7 +29,21 @@ class TrainingQuery(Dataset):
         self.size = len(merged_training)
         
     def __getitem__(self, i):
-        return self.queries[i], self.contents[i], self.targets[i]
+        
+        padding_size = 512
+        tokenized_text = tokenizer.tokenize(self.contents[i])
+
+        if len(tokenized_text) > padding_size:
+            tokenized_text = tokenized_text[:padding_size]
+        else:
+            tokenized_text = tokenized_text + ['[PAD]'] * (padding_size - len(tokenized_text))
+
+        indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+        segments_tensors = torch.zeros(padding_size).long()
+        tokens_tensor = torch.tensor(indexed_tokens)
+        
+        return self.queries[i], tokens_tensor, segments_tensors, self.targets[i]
     
     def __len__(self):
         return self.size
