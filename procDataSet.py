9c9da565b41b077a81a323b24ba684532d33cbb2
de-wei ye <deweiye6626@gmail.com>
2019-05-27 23:18:41 +0800

    [mod] dont use dataframe to speedup

--- procDataSet.py
+++ procDataSet.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np
+from scipy import sparse
 
 
 class TrainingQuery(Dataset):
@@ -42,27 +43,24 @@ class TrainingQueryAll(Dataset):
         self.queries = torch.from_numpy(query_embedded)
         
         # data to indices
-        raw_training_data['Query'] = raw_training_data['Query'].apply(query_list.index)
-        raw_training_data['News_Index'] = raw_training_data['News_Index'].apply(lambda x: int(x[5:]))
         self.indices = list(product(range(len(query_list)), range(self.contents.shape[0])))
         self.size = len(self.indices)
-        self.targets = raw_training_data
+        self.targets = sparse.coo_matrix(
+            (
+                raw_training_data['Relevance'] ,(
+                    raw_training_data['Query'].apply(query_list.index), 
+                    raw_training_data['News_Index'].apply(lambda x: int(x[5:]))
+                )
+            ), 
+            (len(query_list), self.contents.shape[0])
+        ).toarray()
         
     def __getitem__(self, i):     
         j, k = self.indices[i]
-        df = self.targets
-        target = df.loc[(df['Query'] == j) & (df['News_Index'] == k)]['Relevance']
-        if target.empty:
-            # 不相關的項目
-            target = 0
-        else:
-            # 相關: 0~3 provided by TD.csv
-            target = target.values
-        return self.queries[j], self.contents[k], torch.tensor(target)
+        return self.queries[j], self.contents[k], torch.tensor(self.targets[j, k])
     
     def __len__(self):
         return self.size
-    
     
 
 class TestQuery(Dataset):
