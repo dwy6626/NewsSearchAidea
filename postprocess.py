c062ef7e7bd346774eb4d51c15283da598d872b8
yiminchi <47897834+yiminchi@users.noreply.github.com>
2019-06-10 17:30:28 +0800

    debug

--- postprocess.py
+++ postprocess.py
@@ -14,7 +14,7 @@ candidate = np.loadtxt(args.ans_path, dtype=str, delimiter=',')
 
 for query in range(20):
     for i in range(preds.shape[0]):
-        if preds[query,i]==0:
+        if preds[i,query]==0:
             candidate[query+1] = np.concatenate((candidate[query+1, :i+1], candidate[query+1, i+2], candidate[query+1, i+1]), axis=1)
 
 
